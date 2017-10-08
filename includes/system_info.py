1c53781ac93471f1bc67b6249c94c236fc1b9a03
dinpo <dinpo@noreply.com>
2017-10-08 23:13:06 +0000

    Updated systeminfo for newer version of psutil

--- includes/system_info.py
+++ includes/system_info.py
@@ -12,13 +12,13 @@ network_interface = 'ppp0'
 
 def query_system_status():
     """Query global mem, CPU, disk, and network (if present)"""
-    uptime = int(time.time() - psutil.BOOT_TIME)
+    uptime = int(time.time() - psutil.boot_time())
     uptime = str(datetime.timedelta(seconds=uptime))
     try:
-        used_mem = psutil.virtmem_usage().percent
+        used_mem = psutil.swap_memory().percent
         used_cpu = psutil.cpu_percent(percpu=False) #N.B Only looking at one CPU
         used_disk = psutil.disk_usage('/').percent
-        total_network = psutil.network_io_counters(pernic=True)
+        total_network = psutil.net_io_counters(pernic=True)
         network_sent,network_rcvd=-1,-1
         if network_interface in total_network:
             network_sent = round(total_network[network_interface].bytes_sent/1024.0/1024.0,2)
@@ -33,13 +33,13 @@ def fetch_busy_processes():
     now = int(time.time())
     busy_pids = []
 #
-    for p in psutil.get_pid_list():
+    for p in psutil.pids():
         try:
             pid = psutil.Process(p)
-            p_cpu = pid.get_cpu_percent()
-            p_mem = pid.get_memory_percent()
-            p_name = pid.name
-#           logging.debug( "Name: %s, CPU: %s, MEM: %s" %(p_name, p_cpu, p_mem))
+            p_cpu = pid.cpu_percent()
+            p_mem = pid.memory_percent()
+            p_name = pid.name()
+            logging.debug( "Name: %s, CPU: %s, MEM: %s" %(p_name, p_cpu, p_mem))
             if p_cpu >= cpu_threshold or p_mem >= mem_threshold:
                 busy_pid = {'name': p_name, 'pid':p, 'cpu' : p_cpu, 'mem' : p_mem} 
                 busy_pids.append( busy_pid )
